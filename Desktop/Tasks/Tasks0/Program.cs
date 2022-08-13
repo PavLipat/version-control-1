b6e228484ad5cf766315bb7bc2ffe06e752a9c41
Pavel <pavel-lipat@yandex.ru>
2022-08-13 13:05:10 +0300

    Нахождение квадрата

---
+++ Desktop/Tasks/Tasks0/Program.cs
@@ -0,0 +1,7 @@
+﻿// Напишите программу, которая на вход
+// принимает число, а на выход его квадрат
+
+Console.Write("Введите целое число : ") ;
+int number = Convert.ToInt32(Console.ReadLine()) ;
+int square = number * number ;
+Console.WriteLine($"квадрат числа {number} = {square}");
