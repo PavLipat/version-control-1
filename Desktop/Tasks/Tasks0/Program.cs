4f3079a3fd4362ec96f557393fe7c8c35e63a096
ЛипатовПВ <109306818+PavLipat@users.noreply.github.com>
2022-08-29 09:44:41 +0300

    Delete Desktop/Tasks directory

--- Desktop/Tasks/Tasks0/Program.cs
+++
@@ -1,7 +0,0 @@
-﻿// Напишите программу, которая на вход
-// принимает число, а на выход его квадрат
-
-Console.Write("Введите целое число : ") ;
-int number = Convert.ToInt32(Console.ReadLine()) ;
-int square = number * number ;
-Console.WriteLine($"квадрат числа {number} = {square}");
